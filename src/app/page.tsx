18e1b325454e705c169c9983acb8ff79ecd0faa8
mibu119 <horang296@gmail.com>
2023-07-09 22:07:21 +0900

    💄 배경 넣기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl text-sky-400">Hello</h1>
+      <div
+        className="w-full h-screen bg-cover bg-center ..."
+        style={{
+          backgroundImage: `url('/image/beach.jpg')`,
+        }}
+      ></div>
+    </main>
   );
 }
