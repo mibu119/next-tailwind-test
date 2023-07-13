df49067f3c1cb7f3d2657310fe0ec97dd6d44340
mibu119 <horang296@gmail.com>
2023-07-13 22:11:28 +0900

    🌱 💄 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl text-sky-400">Hello</h1>
-      <div
-        className="w-full h-screen bg-cover bg-center ..."
-        style={{
-          backgroundImage: `url('/image/beach.jpg')`,
-        }}
-      ></div>
+      <div className="w-full h-screen bg-cover bg-center bg-[url('/image/beach.jpg')]"></div>
     </main>
   );
 }
